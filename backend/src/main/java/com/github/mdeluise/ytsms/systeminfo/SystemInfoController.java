eb51bcbf03ee84bf4155a62b5992dee60ad1defa
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2023-05-17 09:12:58 +0200

    fix(open-api): Specify no authentication needed for system info

--- backend/src/main/java/com/github/mdeluise/ytsms/systeminfo/SystemInfoController.java
+++ backend/src/main/java/com/github/mdeluise/ytsms/systeminfo/SystemInfoController.java
@@ -2,6 +2,7 @@ package com.github.mdeluise.ytsms.systeminfo;
 
 import com.google.common.base.Strings;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/info")
 @Tag(name = "Info", description = "Endpoints for system info")
+@SecurityRequirements()
 public class SystemInfoController {
     private final String version;
     private final boolean youTubeApiFetchingModeEnabled;
